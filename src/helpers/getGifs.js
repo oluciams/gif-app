fd9180c0441cac30190a26fd35ef03c57ac23aab
olga <oluciams84@gmail.com>
2022-10-24 11:22:28 -0500

    modifica api-key

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) =>{
 
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${category}&limit=10`;  
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=cmVedNVZroYjpG7a0qlQmSzV1WUckVYT&q=${category}&limit=10`;  
   const resp = await fetch( url );
   const {data} = await resp.json();
 
